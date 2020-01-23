34c8139487d0a8f724fc3c8b73668e731c7485d3
HAMZA IBRAHIM <hamze.85@live.com>
2020-01-23 05:32:42 +0200

    Initial Commit

---
+++ responsive_user_ui_lib/src/main/java/com/taskfoundation/responsive_user_ui_lib/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.taskfoundation.responsive_user_ui_lib;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
